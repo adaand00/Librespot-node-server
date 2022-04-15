73616d095e2f905cc1421fc3c472e8061a244edd
Adam Anderson <adaand00@gmail.com>
2022-04-15 16:15:18 +0100

    Snapcast config

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@
 sudo apt update
 sudo apt upgrade
 
+# Install nodejs
+sudo apt install nodejs npm curl iptables
+npm install
+
 # Install raspotify
-sudo apt install raspotify
+curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
 # Install service file
 sudo cp ./service/librespot-node-api.service /usr/lib/systemd/system/librespot-node-api.service
@@ -14,12 +18,22 @@ sudo systemctl enable librespot-node-api.service
 
 # Install raspotify conf
 sudo cp ./raspotify/conf /etc/raspotify/conf
+sudo service raspotify restart
 
 # Install callback file librespot-handler
 sudo cp ./librespot-handler.sh /usr/local/bin/librespot-handler.sh
 sudo chmod 755 /usr/local/bin/librespot-handler.sh
 
 # Set up routing
-sudo apt install iptables
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8083
 sudo iptables-save
+
+# install drivers 
+wget https://github.com/CASE-Association/case-AudioSystem/raw/master/adau1701-i2s.dtbo
+sudo cp ADAU1701-I2S-Audio-Driver-for-Raspberry-Pi/adau1701-i2s.dtbo /boot/overlays
+
+# Install snapcast
+sudo apt install snapserver snapclient
+
+sudo cp snapcast/snapserver.conf /etc/snapserver.conf
+sudo cp snapcast/snapclient /etc/default/snapclient
