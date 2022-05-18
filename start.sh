3244f297640226a697e6bdb35952bcea6884ff85
Adam Anderson <45564940+adaand00@users.noreply.github.com>
2022-05-18 20:21:25 +0200

    mkfifo

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ sudo service snapserver stop
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8083
 sudo iptables-save
 
+sudo mkfifo /run/snapfifo
+
 sudo service raspotify start
 sudo service snapserver start
 sudo service librespot-node-api start
