1a9e52fe0eb57967277139b7cf4b9736492a3349
Adam Anderson <45564940+adaand00@users.noreply.github.com>
2022-05-18 20:03:19 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+sudo service raspotify stop
+sudo service librespot-node-api stop
+sudo service snapserver stop
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8083
+sudo iptables-save
+
+sudo service raspotify start
+sudo service snapserver start
+sudo service librespot-node-api start
